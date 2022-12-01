5a873a2b0c22667875767c9e98ae16c1989e20d1
Nilisofficiel <versselsiyapze@gmail.com>
2022-11-30 23:55:05 -0800

    plashscreen_fond

--- gestionHotel/gestionHotel/Form1.Designer.cs
+++ gestionHotel/gestionHotel/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // 
             // guna2PictureBox1
             // 
-            this.guna2PictureBox1.BorderRadius = 15;
+            this.guna2PictureBox1.BorderRadius = 20;
             this.guna2PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2PictureBox1.Image = global::gestionHotel.Properties.Resources.fond_gi3;
             this.guna2PictureBox1.ImageRotate = 0F;
@@ -57,7 +57,7 @@
             this.label1.BackColor = System.Drawing.Color.BlueViolet;
             this.label1.Font = new System.Drawing.Font("Script MT Bold", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.AntiqueWhite;
-            this.label1.Location = new System.Drawing.Point(45, 393);
+            this.label1.Location = new System.Drawing.Point(40, 388);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(479, 11);
             this.label1.TabIndex = 18;
@@ -70,7 +70,7 @@
             this.guna2HtmlLabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.guna2HtmlLabel1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.MistyRose;
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(221, 172);
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(222, 168);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
             this.guna2HtmlLabel1.Size = new System.Drawing.Size(163, 29);
             this.guna2HtmlLabel1.TabIndex = 17;
@@ -82,7 +82,7 @@
             this.guna2PictureBox2.FillColor = System.Drawing.Color.RoyalBlue;
             this.guna2PictureBox2.Image = global::gestionHotel.Properties.Resources.Sans_titre_2_Récupéré;
             this.guna2PictureBox2.ImageRotate = 0F;
-            this.guna2PictureBox2.Location = new System.Drawing.Point(12, 52);
+            this.guna2PictureBox2.Location = new System.Drawing.Point(13, 48);
             this.guna2PictureBox2.Name = "guna2PictureBox2";
             this.guna2PictureBox2.Size = new System.Drawing.Size(212, 282);
             this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -96,7 +96,7 @@
             this.progressBar1.BorderRadius = 10;
             this.progressBar1.BorderThickness = 2;
             this.progressBar1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
-            this.progressBar1.Location = new System.Drawing.Point(22, 356);
+            this.progressBar1.Location = new System.Drawing.Point(23, 352);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.ProgressColor = System.Drawing.Color.BlueViolet;
             this.progressBar1.ProgressColor2 = System.Drawing.Color.BlueViolet;
@@ -110,7 +110,7 @@
             this.guna2HtmlLabel2.BackColor = System.Drawing.Color.AntiqueWhite;
             this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.BlueViolet;
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(22, 22);
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(23, 18);
             this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
             this.guna2HtmlLabel2.Size = new System.Drawing.Size(147, 31);
             this.guna2HtmlLabel2.TabIndex = 15;
